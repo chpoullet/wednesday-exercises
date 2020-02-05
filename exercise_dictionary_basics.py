3634aba73166d41d49d99475c6ee931a67391abd
Charles Poullet <CPoullet@spartaglobal.com>
2020-02-05 16:46:43 +0000

    Completed dictionary basics exercise

---
+++ exercise_dictionary_basics.py
@@ -0,0 +1,29 @@
+# 1
+
+story1 = {
+    'Start': 'Hello',
+    'Middle': 'How are you?',
+    'End': 'Goodbye',
+}
+
+# 2
+print(story1)
+
+# 3
+print(type(story1))
+
+# 4
+print(story1.keys())
+
+# 5
+print(story1.values())
+
+# 6
+print(story1['Start'])
+print(story1['Middle'])
+print(story1['End'])
+
+# 7
+story1.update(Villain='Joker')
+
+print(story1)

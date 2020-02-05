f9c704b0e6439535a7b7bce4a93e003e4e8dda35
Charles Poullet <CPoullet@spartaglobal.com>
2020-02-05 16:35:58 +0000

    Initial commit

---
+++ exercise_list_basics.py
@@ -0,0 +1,45 @@
+# 1
+
+bucket_list = [
+    'Travel to USA',
+    'Skydive',
+    'Attend the olympics',
+    'Visit the Seven Wonders of the World',
+    'Better at Python',
+    'Learn an instrument',
+    'Buy my first house',
+    'Own a pet',
+    'Learn to scuba dive',
+    'Bungee jump'
+]
+# 2
+
+print(bucket_list)
+
+# 3
+
+print(len(bucket_list))
+
+# 4
+
+print(bucket_list[0])
+
+# 5
+
+print(bucket_list[1])
+
+# 6
+
+bucket_list.append('Visit South America')
+
+# 7
+
+print(bucket_list[-1])
+
+# 8
+
+bucket_list[1] = 'Go skiing'
+
+# 9
+
+print(bucket_list)
